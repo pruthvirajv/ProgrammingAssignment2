b77c978b52a48f24bd0fc98127235de5d68472fa
Pruthvi Raj Venkatesh <34631449+pruthvirajv@users.noreply.github.com>
2018-10-14 16:41:34 +0100

    Updates Function
    Updates the function body

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,33 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  matInv <- NULL
+  setMat <- function(y) {
+    x <<- y
+    matInv <<- NULL
+  }
+  getMat <- function() x
+  setInv <- function(inv) matInv <<- inv
+  getInv <- function() matInv
+  list(set = setMat, get = getMat,
+       setInv = setInv,
+       getInv = getInv)
+  
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  matInv <- x$getInv()
+  if(!is.null(matInv)) {
+    message("getting cached data")
+    return(matInv)
+  }
+  data <- x$get()
+  matInv <- solve(data, ...)
+  x$setInv(matInv)
+  matInv
 }
